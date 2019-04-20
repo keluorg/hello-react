152ae78506732ee3d40ac6a083ccaeaed49f7b2a
血衫非弧 <admin@kelu.org>
2019-04-20 13:41:09 +0800

    [update] 7. 条件渲染1

--- src/index.js
+++ src/index.js
@@ -3,14 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 // import Clock from './Clock';
-import Toggle from './Toggle';
+// import Toggle from './Toggle';
 import * as serviceWorker from './serviceWorker';
 
+function UserGreeting(props) {
+    return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+    return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+        return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+}
+
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    );
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    );
+}
+
 ReactDOM.render(
-    <Toggle />,
+    // Try changing to isLoggedIn={true}:
+    <Greeting isLoggedIn={true} />,
     document.getElementById('root')
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
