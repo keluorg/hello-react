f2321ca53f277176a0e3e7a556d4be9a4c7b6729
血衫非弧 <admin@kelu.org>
2019-04-20 08:55:21 +0800

    [update] easy clock

--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world! It is {new Date().toLocaleTimeString()}.</h1>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root2'));
+}
+
+setInterval(tick, 1000);
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
