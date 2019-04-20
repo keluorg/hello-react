4516bf1259557a4c95b4c97a8aec7ee6c02703f4
血衫非弧 <admin@kelu.org>
2019-04-20 10:49:20 +0800

    [update] clock class 1

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Clock from './Clock';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-function tick() {
-    const element = (
-        <div>
-            <h1>Hello, world! It is {new Date().toLocaleTimeString()}.</h1>
-        </div>
-    );
-    ReactDOM.render(element, document.getElementById('root2'));
-}
-
-setInterval(tick, 1000);
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
