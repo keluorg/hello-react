e787dd6348a609f0ae27b802e4b2bb35765e5b54
血衫非弧 <admin@kelu.org>
2019-04-22 10:40:35 +0800

    [update] 12. react 哲学 2 单项数据绑定

--- src/FilterableProductTable.js
+++ src/FilterableProductTable.js
@@ -5,11 +5,11 @@ class ProductCategoryRow extends React.Component {
         const category = this.props.category;
         return (
             <tr>
-            <th colSpan="2">
-            {category}
-            </th>
+                <th colSpan="2">
+                    {category}
+                </th>
             </tr>
-    );
+        );
     }
 }
 
@@ -18,80 +18,111 @@ class ProductRow extends React.Component {
         const product = this.props.product;
         const name = product.stocked ?
             product.name :
-    <span style={{color: 'red'}}>
+            <span style={{color: 'red'}}>
         {product.name}
-    </span>;
+      </span>;
 
         return (
             <tr>
-            <td>{name}</td>
-            <td>{product.price}</td>
-        </tr>
-    );
+                <td>{name}</td>
+                <td>{product.price}</td>
+            </tr>
+        );
     }
 }
 
 class ProductTable extends React.Component {
     render() {
+        const filterText = this.props.filterText;
+        const inStockOnly = this.props.inStockOnly;
+
         const rows = [];
         let lastCategory = null;
 
         this.props.products.forEach((product) => {
+            if (product.name.indexOf(filterText) === -1) {
+                return;
+            }
+            if (inStockOnly && !product.stocked) {
+                return;
+            }
             if (product.category !== lastCategory) {
+                rows.push(
+                    <ProductCategoryRow
+                        category={product.category}
+                        key={product.category} />
+                );
+            }
             rows.push(
-            <ProductCategoryRow
-            category={product.category}
-            key={product.category} />
-        );
-        }
-        rows.push(
-        <ProductRow
-        product={product}
-        key={product.name} />
-    );
-        lastCategory = product.category;
-    });
+                <ProductRow
+                    product={product}
+                    key={product.name}
+                />
+            );
+            lastCategory = product.category;
+        });
 
         return (
             <table>
-            <thead>
-            <tr>
-            <th>Name</th>
-            <th>Price</th>
-            </tr>
-            </thead>
-            <tbody>{rows}</tbody>
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+                </thead>
+                <tbody>{rows}</tbody>
             </table>
-    );
+        );
     }
 }
 
 class SearchBar extends React.Component {
     render() {
+        const filterText = this.props.filterText;
+        const inStockOnly = this.props.inStockOnly;
+
         return (
             <form>
-            <input type="text" placeholder="Search..." />
-            <p>
-            <input type="checkbox" />
-            {' '}
-        Only show products in stock
-        </p>
-        </form>
-    );
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={filterText} />
+                <p>
+                    <input
+                        type="checkbox"
+                        checked={inStockOnly} />
+                    {' '}
+                    Only show products in stock
+                </p>
+            </form>
+        );
     }
 }
 
 class FilterableProductTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterText: 'ball',
+            inStockOnly: false
+        };
+    }
+
     render() {
         return (
             <div>
-            <SearchBar />
-            <ProductTable products={this.props.products} />
-        </div>
-    );
+                <SearchBar
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                />
+                <ProductTable
+                    products={this.props.products}
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                />
+            </div>
+        );
     }
 }
-
-
 
 export default FilterableProductTable;
